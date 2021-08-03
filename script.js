6bbd76f17d50dc4011ea91fa0da56bd034f4bf33
Sumi jaiswal <jaissumi1402@gmail.com>
2021-08-03 12:50:58 +0530

    script created

---
+++ script.js
@@ -0,0 +1,86 @@
+
+function formClear() {
+$("#title").val("");
+$("#description").val("");
+
+}
+function getAndUpdate(){
+    console.log("Updating List...");
+    tit = document.getElementById('title').value;
+    desc = document.getElementById('description').value;
+    if (localStorage.getItem('itemsJson')==null){
+        itemJsonArray = [];
+        itemJsonArray.push([tit, desc]);
+        formClear();
+        localStorage.setItem('itemsJson', JSON.stringify(itemJsonArray))
+    }
+    else{
+        itemJsonArrayStr = localStorage.getItem('itemsJson')
+        itemJsonArray = JSON.parse(itemJsonArrayStr);
+        itemJsonArray.push([tit, desc]);
+        formClear();
+        localStorage.setItem('itemsJson', JSON.stringify(itemJsonArray))
+    }
+    update();
+}
+
+function update(){
+    if (localStorage.getItem('itemsJson')==null){
+        itemJsonArray = []; 
+        localStorage.setItem('itemsJson', JSON.stringify(itemJsonArray))
+    } 
+    else{
+        itemJsonArrayStr = localStorage.getItem('itemsJson')
+        itemJsonArray = JSON.parse(itemJsonArrayStr); 
+    }
+    // Populate the table
+    let tableBody = document.getElementById("tableBody");
+    let str = "";
+    itemJsonArray.forEach((element, index) => {
+        str += `
+        <tr>
+        <th scope="row">${index + 1}</th>
+        <td>${element[0]}</td>
+        <td>${element[1]}</td> 
+        <td><button class="btn btn-sm btn-primary" onclick="deleted(${index})">Delete</button></td> 
+        </tr>`; 
+    });
+    tableBody.innerHTML = str;
+}
+add = document.getElementById("add");
+add.addEventListener("click", getAndUpdate);
+update();
+function deleted(itemIndex){
+    console.log("Delete", itemIndex);
+    itemJsonArrayStr = localStorage.getItem('itemsJson')
+    itemJsonArray = JSON.parse(itemJsonArrayStr);
+    // Delete itemIndex element from the array
+    itemJsonArray.splice(itemIndex, 1);
+    localStorage.setItem('itemsJson', JSON.stringify(itemJsonArray));
+    update();
+
+}
+function clearStorage(){
+    if (confirm("Do you areally want to clear?")){
+    console.log('Clearing the storage')
+    localStorage.clear();
+    update()
+    }
+    
+}
+const dateDiv = document.querySelector('#today-date')
+let today = new Date()
+let month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+let day = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
+let currentMonth = month[today.getMonth()]
+let currentDay = day[today.getDay()]
+let currentDate = today.getDate()
+if (currentDate <= 10) {
+currentDate = '0' + currentDate;
+}
+if (currentMonth <= 10) {
+currentMonth = '0' + currentMonth;
+}
+const displayDate = currentDay + ',' + ' ' + currentMonth + ' ' + currentDate
+dateDiv.innerHTML= displayDate
